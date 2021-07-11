009d262d3f12b54b35660b252af1511c303f1605
Gahraman Akbarov <74054688+Bakubus@users.noreply.github.com>
2021-07-12 00:41:11 +0200

    Last Update(updating user profile)

--- djangoproject/blog/views.py
+++ djangoproject/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blof Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-{
-        'author': 'CollyMS',
-        'title': 'BloG Post 13',
-        'content': 'Second post content',
-        'date_posted': 'july 17, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
